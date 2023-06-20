429cd967ebc8d4a331be2484a717896df0a8110f
Tatiane Sala <tatianedleal@gmail.com>
2023-06-20 11:23:04 -0400

    add formgroup to post component

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  @Input() employeeId!: number;
 
+  postForm!: FormGroup;
+  message!: string;
+
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.postForm = this._fb.group({
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    const message = this.postForm.value.message;
+  }
 }
--- src/app/components/post/post.module.ts
+++ src/app/components/post/post.module.ts
@@ -5,10 +5,19 @@ import { CommentModule } from '../comment/comment.module';
 import { AvatarModule } from '../avatar/avatar.module';
 
 import { PostComponent } from './post.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [PostComponent],
-  imports: [CommonModule, CommentModule, AvatarModule],
+  imports: [
+    CommonModule,
+    CommentModule,
+    AvatarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
   exports: [PostComponent],
 })
 export class PostModule {}
