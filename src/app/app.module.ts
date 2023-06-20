33751cd3fd4765f14826d015fb142d82372c5962
Tatiane Sala <tatianedleal@gmail.com>
2023-06-20 11:18:51 -0400

    clean up on app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HeaderModule } from './components/header/header.module';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, HeaderModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeaderModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
