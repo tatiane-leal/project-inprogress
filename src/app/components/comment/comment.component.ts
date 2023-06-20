adb1c4885c7f29cb86fd38736b176449bdbc94e5
Tatiane Sala <tatianedleal@gmail.com>
2023-06-20 11:19:59 -0400

    get comment by person id

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Comment } from 'src/app/models/comment.interface';
+import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.scss']
+  styleUrls: ['./comment.component.scss'],
 })
-export class CommentComponent {
+export class CommentComponent implements OnInit {
+  @Input() employeeId!: number;
 
+  comments!: Comment[];
+
+  constructor(private _commentService: CommentService) {}
+
+  ngOnInit(): void {
+    this._commentService
+      .getCommentByBirthdayPersonId(this.employeeId)
+      .subscribe((data) => {
+        this.comments = data;
+      });
+  }
 }
