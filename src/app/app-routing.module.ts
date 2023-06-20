b41f0decc92db044f1fc56863a5d16d93f672d24
Tatiane Sala <tatianedleal@gmail.com>
2023-06-20 11:18:05 -0400

    update app routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    loadChildren: () =>
+      import('./components/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'user/:id',
+    component: UserComponent,
+    loadChildren: () =>
+      import('./components/user/user.module').then((m) => m.UserModule),
+  },
+
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
